7a2c1816e7c56c053bd7605456775840aadc2c41
hrasamoe <herysamuelpljv@gmail.com>
2026-06-26 11:22:54 +0300

    Feat Allocation and print result

--- dynamic_2d_array.cpp
+++ dynamic_2d_array.cpp
@@ -11,5 +11,21 @@ int main()
         int k;
         cin >> k;
         a[i] = new int[k];
+        for (int j = 0; j < k; j++)
+        {
+            cin >> a[i][j];
+        }
     }
+    while (q > 0)
+    {
+        int x,y;
+        cin >> x >> y;
+        printf("%d\n", a[x][y]);
+        q--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        delete[] a[i];
+    }
+    delete[] a;
 }
