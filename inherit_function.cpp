50dd5a1c07ec18ae84638d52ec74e9e4b457ab4c
Heritiana <herysamuelpljv@gmail.com>
2026-07-02 16:44:29 +0300

    Feat: Enhance class D to implement update_val function for processing new_val with A, B, and C's func

--- inherit_function.cpp
+++ inherit_function.cpp
@@ -72,12 +72,11 @@ class C
         }
 };
 
-class D 
+class D: public A , public B , public C 
 {
 
 	int val;
 	public:
-		//Initially val is 1
 		 D()
 		 {
 		 	val = 1;
@@ -87,8 +86,24 @@ class D
 		 //Implement this function
 		 void update_val(int new_val)
 		 {
-
-			
+            while (new_val > 1)
+            {
+                if (new_val % 2 == 0)
+                {
+                    new_val /= 2;
+                    A::func(val);
+                }
+                if (new_val % 3 == 0)
+                {
+                    new_val /= 3;
+                    B::func(val);
+                }
+                if (new_val % 5 == 0)
+                {
+                    new_val /= 5;
+                    C::func(val);
+                }
+            }
 		 }
 		 //For Checking Purpose
 		 void check(int); //Do not delete this line.
